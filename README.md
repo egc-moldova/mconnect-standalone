b8f391cd4230e7eadfbd71fa4e863a5a96c2896b
egc-moldova <office@egov.md>
2016-04-21 14:59:43 +0300

    fix

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # mconnect-standalone
+
+# install git and clone repository
+
+apt-get install git -y
+git clone --depth=1 https://github.com/egc-moldova/mconnect-standalone
+cd mconnect-standalone
+./main.sh
--- main.sh
+++ main.sh
@@ -1,15 +1,10 @@
-# install ansible and git
+# install ansible
 
 apt-get install software-properties-common -y
 apt-add-repository ppa:ansible/ansible -y
 apt-get update
-apt-get install ansible git -y
-
-# download mconnect-standalone
-
-git clone --depth=1 https://github.com/egc-moldova/mconnect-standalone
+apt-get install ansible -y
 
 # run installation script
 
-cd mconnect-standalone
 ansible-playbook -i inventory site.yml
